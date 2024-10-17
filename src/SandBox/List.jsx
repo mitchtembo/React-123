f04532d43eb43d61ca5b9d6805cf623880319092
mitchtembo <mitchtembo@gmail.com>
2024-10-17 12:40:48 +0200

    Destructing An Object

--- src/SandBox/List.jsx
+++ src/SandBox/List.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
+const userInfo = [
+    {
+        name: "Mitchel Tembo",
+        location: "Harare Zimbabwe",
+        role: "Software Developer"
+    },
+    {
+        name: "Mutsawashe Tembo",
+        location: "Harare Zimbabwe",
+        role: "DevOps Engineer"
+    },
+    {
+        name: "Mukudzeishe Tembo",
+        location: "Salt Lake City U.S.",
+        role: "Frontend Dev"
+    }
+]
 
 const List = () => {
-    const numbers = [1,2,3,4,5]
   return (
     <div>
-        {numbers.map((number) => (
-          <ul key={number}>
-                <li>{number}</li>
+        {userInfo.map(({name,location,role}) => (
+            <ul key={Math.random()}>
+                <li>name:{name}</li>
+                <li>location:{location}</li>
+                <li>role:{role}</li>
             </ul>
         ))}
     </div>
